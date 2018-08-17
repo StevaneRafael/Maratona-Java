f47dcf3e5b1dfa10b9ccda3c3864c66651912dec
StevaneRafael <stevanerafael@gmail.com>
2018-08-17 13:34:41 -0300

    Tipos primitivos, convencoes de codigo e comentarios

---
+++ src/ImprimindoVariaveis.java
@@ -0,0 +1,18 @@
+public class ImprimindoVariaveis {
+
+    //Isto é um comentário
+
+    /*
+    * Comentário em bloco
+    *
+    * */
+
+    /***
+     * Esse é o método main
+     * @param args parametro de entrada do metodo main
+     */
+    public static void main(String[] args){
+        int idadeDosPaisParaCadastro = 10;
+        System.out.println("A idade é: " + idadeDosPaisParaCadastro);
+    }
+}
